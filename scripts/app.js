c35d62d1d884bb10c27a5acd66cf60cae00faa5d
Alexey Dudarev <dudarev.av@gmail.com>
2013-10-24 07:45:19 -0700

    modifications : bottom 0, drag

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,16 @@ $(function() {
 
     app.windowWidget = function(window){
 
-        window.draggable();
+        window.draggable({
+            drag: function(event, ui){
+                var position = ui.position,
+                helper = ui.helper;
+                if(position.top + helper.height() > 0 ){
+                    this.stop();
+                    event.stop();
+                }
+            }
+        });
 
         window.find('.title').mousedown(function(){
             app.windowWidget.activateWindow(window,"enable");
